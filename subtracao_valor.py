9243a23ccfef51b9a94f832eacd446d86ca66316
Marcos Silva <marcosilva968@gmail.com>
2018-06-25 07:56:37 -0300

    Cálculo de subtracao em Python

---
+++ subtracao_valor.py
@@ -0,0 +1,10 @@
+#Subtração de valores
+
+#Input do primeiro valor
+valor1 = int(input('Digite o primeiro valor: '))
+
+#Input do segundo valor
+valor2 = int(input('Digite o segundo valor: '))
+
+#Subtração dos valores e apresentação na tela
+print('A subtração dos valores e igual a ', valor1 - valor2)
