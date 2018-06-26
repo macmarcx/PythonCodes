3568a76232636c9742a11240d5f7167d718c7ed6
Marcos Silva <marcosilva968@gmail.com>
2018-06-26 08:05:40 -0300

    Calculo de Divisao em Python

---
+++ divisao_valor.py
@@ -0,0 +1,10 @@
+#Divisão de valores
+
+#Input do primeiro valor
+valor1 = int(input('Digite o primeiro valor: '))
+
+#Input do segundo valor
+valor2 = int(input('Digite o segundo valor: '))
+
+#Divisão dos valores e apresentação na tela
+print('A Divisão dos valores e igual a ', valor1 / valor2)
