63e05aa7a236746a832f0a560d4004fd4043c0b2
Marcos Silva <marcosilva968@gmail.com>
2018-07-07 20:24:34 -0300

    Calculo de porcentagem em Python

---
+++ porcentagem_valor.py
@@ -0,0 +1,16 @@
+#Porcentagem de valores
+
+#Input do primeiro valor
+valor1 = float(input('Digite o primeiro valor: '))
+
+#Input do segundo valor
+valor2 = float(input('Digite o segundo valor: '))
+
+#Divisão do primeiro valor por 100
+div = valor1 / 100
+
+#Multiplicação da divisão pelo segundo valor
+multi = div * valor2
+
+#Porcentagem dos valores e apresentação na tela
+print('A Porcentagem de ', valor1, ' sobre ', valor2, ' é ', multi)
