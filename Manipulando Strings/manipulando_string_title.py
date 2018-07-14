c35659c37ed75bbce573edf88803e707a0c176b9
Marcos Silva <marcosilva968@gmail.com>
2018-07-14 18:14:46 -0300

    Manipulando string com title

---
+++ Manipulando Strings/manipulando_string_title.py
@@ -0,0 +1,7 @@
+#Manipulando Strings com title
+
+frase = "Manipulando strings em Python"
+
+#Utilizando a função "title" para deixar apenas a primeira letra de cada palavra em maiúsculo
+
+frase.title()
