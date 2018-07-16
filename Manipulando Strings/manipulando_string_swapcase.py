e5502a4832a3f3ce015aedec5d97352d39eaa7d2
Marcos Silva <marcosilva968@gmail.com>
2018-07-15 22:41:10 -0300

    Manipulando string com swapcase

---
+++ Manipulando Strings/manipulando_string_swapcase.py
@@ -0,0 +1,7 @@
+#Manipulando Strings com swapcase
+
+frase = "Manipulando strings em Python"
+
+#Utilizando a função "swapcase" para deixar as letras que forem maiúscula em minúsculo e as minúscula em maiúsculo
+
+frase.swapcase()
