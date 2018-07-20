8e19320f99f9bff2ce1fa444bf3754a5e1040c8e
Marcos Silva <marcosilva968@gmail.com>
2018-07-20 20:22:49 -0300

    Manipulando String com isspace

---
+++ Manipulando Strings/manipulando_string_isspace.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com isspace
+
+#Utilizando a função "isspace" para verificar se uma string tem apenas espaços
+
+#Caso que retorna True
+
+'   '.isspace()
+
+#Caso que retorna False
+
+''.isspace()
