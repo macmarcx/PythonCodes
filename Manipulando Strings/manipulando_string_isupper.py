2a93e15644ad158ae496d808bc2133f209497e25
Marcos Silva <marcosilva968@gmail.com>
2018-07-22 10:47:24 -0300

    Manipulando string com isupper

---
+++ Manipulando Strings/manipulando_string_isupper.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com isupper
+
+#Utilizando a função "isupper" para confirmar se na frase existe apenas letras em maiúsculo.
+
+#Caso que retorna True
+
+'MANIPULANDO STRINGS EM PYTHON'.isupper()
+
+#Caso que retorna False
+
+'Manipulando strings em Python'.isupper()
