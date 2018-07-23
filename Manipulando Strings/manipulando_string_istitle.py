34f0b42d4420dcc84a325cf091ce9b3f2c31c598
Marcos Silva <marcosilva968@gmail.com>
2018-07-23 00:01:58 -0300

    Manipulando string com istitle

---
+++ Manipulando Strings/manipulando_string_istitle.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com istitle
+
+#Utilizando a função "istitle" para verificar se na primeira letra de cada palavra ela é maiúscula
+
+#Caso que retorna True
+
+'Manipulando Strings Em Python'.istitle()
+
+#Caso que retorna False
+
+'manipulando strings em python'.istitle()
