97b8ced1a6e8a780b1c5a0132b76913594ec87b4
Marcos Silva <marcosilva968@gmail.com>
2018-07-24 07:46:36 -0300

    Manipulando string com isdigit

---
+++ Manipulando Strings/manipulando_string_isdigit.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com isdigit
+
+#Utilizando a função "isdigit" para verificar se todos os caracteres são apenas números
+
+#Caso que retorna True
+
+'123456789'.isdigit()
+
+#Caso que retorna False
+
+'M4nipul4nd0StringsEmPyth0n'.isdigit()
