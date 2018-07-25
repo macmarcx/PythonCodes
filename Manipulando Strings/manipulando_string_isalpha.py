a2a4273f20c945951e36353eeefe7076790ed011
Marcos Silva <marcosilva968@gmail.com>
2018-07-25 08:06:23 -0300

    Manipulando string com isalpha

---
+++ Manipulando Strings/manipulando_string_isalpha.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com isalpha
+
+#Utilizando a função "isalpha" para verificar se contém apenas letras na string
+
+#Caso que retorna True
+
+'ManipulandoStringsempython'.isalpha()
+
+#Caso que retorna False - Espaços em branco também conta
+
+'Manipuland0 Strings em pyth0n'.isalpha()
