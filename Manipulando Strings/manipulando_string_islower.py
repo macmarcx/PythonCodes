4dd27412f8906fb08c4b34c37b71d7d68f011e14
Marcos Silva <marcosilva968@gmail.com>
2018-07-26 22:54:34 -0300

    Manipulando string com islower

---
+++ Manipulando Strings/manipulando_string_islower.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com islower
+
+#Utilizando a função "islower" para confirmar se na frase existe apenas letras em minúsculo.
+
+#Caso que retorna True
+
+'manipulando strings em python'.islower()
+
+#Caso que retorna False
+
+'MANIPULANDO STRINGS EM PYTHON'.islower()
