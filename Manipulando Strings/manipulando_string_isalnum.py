b55ec80615d82d018a336ae62ff1a024dfc3b108
Marcos Silva <marcosilva968@gmail.com>
2018-07-28 00:04:47 -0300

    Manipulando string com isalnum

---
+++ Manipulando Strings/manipulando_string_isalnum.py
@@ -0,0 +1,11 @@
+#Manipulando Strings com isalnum
+
+#Utilizando a função "isalnum" para verificar se todos os caracteres são alfa-numericos, sem caracteres especiais
+
+#Caso que retorna True
+
+'M4nipul4nd0StringsEmPyth0n'.isalnum()
+
+#Caso que retorna False - Espaços em branco também conta
+
+'m@nipul@nd0 $trings em python'.isalnum()
