83f62eda6603b0f1c657629273f46db6934c0bc0
Marcos Silva <marcosilva968@gmail.com>
2018-07-29 15:59:02 -0300

    Manipulando string com lower e capitalize

---
+++ Manipulando Strings/manipulando_string_lower_e_capitalize.py
@@ -0,0 +1,7 @@
+#Manipulando Strings com lower e capitalize
+
+frase = "Manipulando strings em Python"
+
+#Utilizando a função "lower e capitalize" para deixar apenas a primeira letra maiúscula e as demais minúscula
+
+frase.lower() .capitalize()
