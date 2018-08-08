cf6125bca2c73f702942cdf7e85399c404babdda
Marcos Silva <marcosilva968@gmail.com>
2018-08-08 13:59:46 -0300

    Atualização de arquivo

--- Manipulando Strings/manipulando_string_replace.py
+++ Manipulando Strings/manipulando_string_replace.py
@@ -1,4 +1,4 @@
-#Manipulando Strings com replace
+#Manipulando Strings com replace 
 
 frase = "Manipulando strings em Python"
 
@@ -9,5 +9,3 @@ frase = frase.replace('strings', 'codigo')
 #Exibindo na tela
 
 frase
-
-
