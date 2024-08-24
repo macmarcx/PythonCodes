eebb108fd9db81aed34260505b180abf2057f611
Marcos Silva <marcosilva968@gmail.com>
2024-08-24 09:10:25 +0100

    Start Visualizer

---
+++ data visualizer/src/main.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import streamlit
+sys.path.append('src')
+sys.path.append(os.path.dirname(__file__))
+from src.ui import run_app
+from ui import run_app
+
+if __name__ == '__main__':
+    run_app()
