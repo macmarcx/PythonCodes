13a52d57fed2ac67f86a085bf4fc6d58684cc6b3
Marcos Silva <marcosilva968@gmail.com>
2024-08-31 08:18:15 +0100

    visualization

---
+++ data visualizer/src/visualizations.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_histogram(data, column):
+    plt.figure(figsize=(10, 6))
+    sns.histplot(data[column], kde=True)
+    plt.title(f'Histogram of {column}')
+    plt.show()
+
+def plot_scatter(data, x_column, y_column):
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=data[x_column], y=data[y_column])
+    plt.title(f'Scatter Plot of {x_column} vs {y_column}')
+    plt.show()
